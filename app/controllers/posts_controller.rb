3045a86d667ec23ee48966c0b40156e4560082f7
brennanfoo <brennan.foo@gmail.com>
2015-10-22 18:07:54 -0400

    Blogs, posts, comments, posts belong to blogs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -24,12 +25,14 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = @blog.posts.new(params[:id])
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to [@blog, @post], notice: 'Post was successfully created.' }
+        format.json { render json: [@blog, @post],
+                             status: :created,
+                             location: [@blog, @post] }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -42,8 +45,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to [@blog, @post], notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@blog, @post] }
       else
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
@@ -56,13 +59,17 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to blog_posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @blog = Post.find(params[:blog])
+    end
+
     def set_post
       @post = Post.find(params[:id])
     end
